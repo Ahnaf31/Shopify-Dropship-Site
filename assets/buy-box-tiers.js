ae6e7a5fd60d4da0b59343805fb70356a1bd2eb3
Ahnaf31 <ahnafarnob31@gmail.com>
2026-07-04 04:20:09 +0600

    added updated version

--- assets/buy-box-tiers.js
+++ assets/buy-box-tiers.js
@@ -1,47 +1,68 @@
 class BuyBoxTiers extends HTMLElement {
   connectedCallback() {
-    this.colorSlots = this.querySelector('[ref="colorSlots"]');
+    this.tiers = this.querySelectorAll("[data-tier]");
     this.template = this.querySelector("template[data-color-select-template]");
     this.addBtn = this.querySelector('[ref="addToCartBtn"]');
     this.errorMsg = this.querySelector('[ref="errorMsg"]');
-    this.tierInputs = this.querySelectorAll('input[type="radio"]');
 
-    this.tierInputs.forEach((input) => {
-      input.addEventListener("change", () => this.renderColorSlots());
+    this.tiers.forEach((tier) => {
+      const input = tier.querySelector('input[type="radio"]');
+      input.addEventListener("change", () => this.onTierChange());
     });
 
+    this.onTierChange();
     this.addBtn.addEventListener("click", () => this.addToCart());
-
-    this.renderColorSlots();
   }
 
-  getSelectedQuantity() {
-    const checked = this.querySelector('input[type="radio"]:checked');
-    return checked ? parseInt(checked.dataset.quantity, 10) : 1;
-  }
+  onTierChange() {
+    this.tiers.forEach((tier) => {
+      const input = tier.querySelector('input[type="radio"]');
+      const colorList = tier.querySelector("[data-color-list]");
+      const isChecked = input.checked;
 
-  renderColorSlots() {
-    const qty = this.getSelectedQuantity();
-    this.colorSlots.innerHTML = "";
-    for (let i = 0; i < qty; i++) {
-      const clone = this.template.content.cloneNode(true);
-      this.colorSlots.appendChild(clone);
-    }
-    this.tierInputs.forEach((input) => {
-      input
-        .closest(".buy-box-tiers__tier")
-        .classList.toggle("buy-box-tiers__tier--checked", input.checked);
+      tier.classList.toggle("buy-box-tiers__tier--checked", isChecked);
+
+      if (isChecked) {
+        const qty = parseInt(input.dataset.quantity, 10);
+        colorList.innerHTML = "";
+        for (let i = 0; i < qty; i++) {
+          const clone = this.template.content.cloneNode(true);
+          colorList.appendChild(clone);
+        }
+        colorList.querySelectorAll("[data-color-select]").forEach((select) => {
+          select.addEventListener("change", () => this.updateSwatch(select));
+          this.updateSwatch(select);
+        });
+      } else {
+        colorList.innerHTML = "";
+      }
     });
+  }
+
+  updateSwatch(select) {
+    const option = select.selectedOptions[0];
+    const row = select.closest(".buy-box-tiers__color-row");
+    const img = row ? row.querySelector("[data-swatch-img]") : null;
+    if (img && option && option.dataset.swatchSrc) {
+      img.src = option.dataset.swatchSrc;
+    }
+  }
+
+  getActiveColorsContainer() {
+    return this.querySelector(
+      ".buy-box-tiers__tier--checked [data-color-list]",
+    );
   }
 
   async addToCart() {
     this.errorMsg.hidden = true;
-    const selects = this.colorSlots.querySelectorAll("[data-color-select]");
+    const container = this.getActiveColorsContainer();
+    const selects = container.querySelectorAll("[data-color-select]");
     const items = [];
 
     for (const select of selects) {
       const option = select.selectedOptions[0];
-      const variantId = option?.dataset.variantId;
+      const variantId = option ? option.dataset.variantId : null;
       if (!variantId) {
         this.errorMsg.textContent =
           "Please select an available color for each unit.";
